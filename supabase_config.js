c07f2270e0e866f060a2214bab839bc33e978ea9
MenboLD <menbo.luckyd@gmail.com>
2025-12-20 18:11:45 +0900

    Add files via upload

--- supabase_config.js
+++ supabase_config.js
@@ -1,4 +1,4 @@
 // supabase_config.js
 // Paste your real values here (GitHub Pages: anon key in client is OK when RLS is configured)
 window.LD_SUPABASE_URL = "https://teggcuiyqkbcvbhdntni.supabase.co";
-window.LD_SUPABASE_ANON_KEY = "PASTE_YOUR_REAL_ANON_KEY_HERE";
+window.LD_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlZ2djdWl5cWtiY3ZiaGRudG5pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1OTIyNzUsImV4cCI6MjA4MDE2ODI3NX0.R1p_nZdmR9r4k0fNwgr9w4irkFwp-T8tGiEeJwJioKc";
